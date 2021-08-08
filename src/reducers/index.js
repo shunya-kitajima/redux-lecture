24df5b71d061b6e5e92cb836b8433e954430f255
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-08-08 22:50:53 +0900

    combineReducersを使い、複数のreducerを結合する

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+import reducerMeat from "./reducerMeat";
+import reducerVegetable from "./reducerVegetable";
+
+const rootReducer = combineReducers({ reducerMeat, reducerVegetable });
+
+export default rootReducer;
